3703422c294204f17adce48974d40b9b3b281590
Jonathan Pohahau <jonathanpohahau@Jonathans-MBP.attlocal.net>
2023-01-18 12:59:11 -0800

    reworking on my page

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,85 +63,85 @@ searchBtn.on("click", function(event) {
     console.log(currentWeather);
     console.log(forecast);
 
-function displayTodayWeather() {
-    
-    if (searchBtn) {
-        cityTitle.text(cityInput.val())
-        fetch(currentWeather)
-            .then(function (response) {
-            return response.json();
-            //   console.log(response)
-        })
-            .then(function (data) {
-                cityTitle.textContent = cityInput.val();
-                temp.text("Temp: " + data.main.temp + " °F") 
-                wind.text("Wind: " + data.wind.speed + " mph")  
-                humidity.text("Humidity: " + data.main.humidity + " %") 
-                console.log(data);
-                console.log(temp);
-                console.log(wind);
-                console.log(humidity); 
-                console.log(cityTitle);
-        });
+    function displayTodayWeather() {
         
-
-        currentWeather
-        console.log(currentWeather)
-    } else {
-        console.log("try to enter a city")   
-    }    
-};
-console.log(displayTodayWeather);
-
-
-function displayForecast() {
-    
-    if (searchBtn) {
-        fetch(forecast)
-            .then(function (response) {
+        if (searchBtn) {
+            cityTitle.text(cityInput.val())
+            fetch(currentWeather)
+                .then(function (response) {
                 return response.json();
-                        //   console.log(response)
+                //   console.log(response)
             })
-            .then(function (data) {
-                dateA.text("Date: " + data.list[0].dt_txt) 
-                // iconA.text("Date: " + data.list[0].weather[0].icon + "png") 
-                tempA.text("Temp: " + data.list[0].main.temp + " °F") 
-                windA.text("Wind: " + data.list[0].wind.speed + " mph") 
-                humidityA.text("Humidity: " + data.list[0].main.humidity + " %"); 
-                // console.log(iconA) 
+                .then(function (data) {
+                    cityTitle.textContent = cityInput.val();
+                    temp.text("Temp: " + data.main.temp + " °F") 
+                    wind.text("Wind: " + data.wind.speed + " mph")  
+                    humidity.text("Humidity: " + data.main.humidity + " %") 
+                    console.log(data);
+                    console.log(temp);
+                    console.log(wind);
+                    console.log(humidity); 
+                    console.log(cityTitle);
+            });
+            
 
-                dateB.text("Date: " + data.list[8].dt_txt) 
-                tempB.text("Temp: " + data.list[8].main.temp + " °F") 
-                windB.text("Wind: " + data.list[8].wind.speed + " mph") 
-                humidityB.text("Humidity: " + data.list[8].main.humidity + " %"); 
+            currentWeather
+            console.log(currentWeather)
+        } else {
+            console.log("try to enter a city")   
+        }    
+    };
+    console.log(displayTodayWeather);
 
-                dateC.text("Date: " + data.list[16].dt_txt) 
-                tempC.text("Temp: " + data.list[16].main.temp + " °F") 
-                windC.text("Wind: " + data.list[16].wind.speed + " mph") 
-                humidityC.text("Humidity: " + data.list[16].main.humidity + " %"); 
 
-                dateD.text("Date: " + data.list[24].dt_txt) 
-                tempD.text("Temp: " + data.list[24].main.temp + " °F") 
-                windD.text("Wind: " + data.list[24].wind.speed + " mph") 
-                humidityD.text("Humidity: " + data.list[24].main.humidity + " %"); 
-
-                dateE.text("Date: " + data.list[32].dt_txt) 
-                tempE.text("Temp: " + data.list[32].main.temp + " °F") 
-                windE.text("Wind: " + data.list[32].wind.speed + " mph") 
-                humidityE.text("Humidity: " + data.list[32].main.humidity + " %"); 
-                console.log(data);
-                // console.log(dataA);
-                // console.log(tempA);
-                // console.log(windA);
-                // console.log(humidityA);                   
-            }); 
-
-        forecast
-        console.log(forecast)
-    } else {
-        console.log("try to enter a city")
+    function displayForecast() {
         
-    }
+        if (searchBtn) {
+            fetch(forecast)
+                .then(function (response) {
+                    return response.json();
+                            //   console.log(response)
+                })
+                .then(function (data) {
+                    dateA.text("Date: " + data.list[0].dt_txt) 
+                    // iconA.text("Date: " + data.list[0].weather[0].icon + "png") 
+                    tempA.text("Temp: " + data.list[0].main.temp + " °F") 
+                    windA.text("Wind: " + data.list[0].wind.speed + " mph") 
+                    humidityA.text("Humidity: " + data.list[0].main.humidity + " %"); 
+                    // console.log(iconA) 
+
+                    dateB.text("Date: " + data.list[8].dt_txt) 
+                    tempB.text("Temp: " + data.list[8].main.temp + " °F") 
+                    windB.text("Wind: " + data.list[8].wind.speed + " mph") 
+                    humidityB.text("Humidity: " + data.list[8].main.humidity + " %"); 
+
+                    dateC.text("Date: " + data.list[16].dt_txt) 
+                    tempC.text("Temp: " + data.list[16].main.temp + " °F") 
+                    windC.text("Wind: " + data.list[16].wind.speed + " mph") 
+                    humidityC.text("Humidity: " + data.list[16].main.humidity + " %"); 
+
+                    dateD.text("Date: " + data.list[24].dt_txt) 
+                    tempD.text("Temp: " + data.list[24].main.temp + " °F") 
+                    windD.text("Wind: " + data.list[24].wind.speed + " mph") 
+                    humidityD.text("Humidity: " + data.list[24].main.humidity + " %"); 
+
+                    dateE.text("Date: " + data.list[32].dt_txt) 
+                    tempE.text("Temp: " + data.list[32].main.temp + " °F") 
+                    windE.text("Wind: " + data.list[32].wind.speed + " mph") 
+                    humidityE.text("Humidity: " + data.list[32].main.humidity + " %"); 
+                    console.log(data);
+                    // console.log(dataA);
+                    // console.log(tempA);
+                    // console.log(windA);
+                    // console.log(humidityA);                   
+                }); 
+
+            forecast
+            console.log(forecast)
+        } else {
+            console.log("try to enter a city")
+            
+        }
     
  
 
